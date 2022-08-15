ff30325b687865a135cd44d1ff7269f6beae8c34
Preston Stevenson <preston.stevenson20@gmail.com>
2022-08-14 22:03:04 -0400

    Added second plot, will return when new season info is available

--- DV_Schedule_OU.R
+++ DV_Schedule_OU.R
@@ -104,7 +104,84 @@ df %>%
        subtitle = "Vegas win totals and national TV games for the 2021-22 NBA Season")
 
 ggsave("national_tv_games_over_under.png", p, w = 6, h = 6, dpi = 300, type = 'cairo')
-  
+
+
+
+#Second Visualization
+
+# clean column names, select columsn we care about 
+df <- x %>% 
+  clean_names() %>% 
+  select(game_date, game_id, game_status_text, day, team_name, team_name_2, home_team_time, arena_city, arena_name)
+
+# pivot data long 
+df <- df %>%
+  mutate(teama = team_name,
+         teamb = team_name_2) %>% 
+  pivot_longer(team_name:team_name_2) %>% 
+  mutate(opp = ifelse(value == teama, teamb, teama)) %>% 
+  rename(team = value) %>% 
+  select(-teama, -teamb, -name) 
+
+
+# add win totals for team and opponent 
+df <- left_join(df, win_totals, by = "team") %>% 
+  left_join(., win_totals, by = c("opp" = "team")) %>% 
+  rename(team_ou = ou.x, 
+         opp_ou = ou.y)
+
+# calculate 10-game rolling average of opponent win total 
+df <- df %>% 
+  group_by(team) %>% 
+  mutate(opp_ou_ra = rollmean(opp_ou, k = 10, na.pad = TRUE, align = 'right'), 
+         gameno = row_number()) %>% 
+  ungroup() 
+
+# add team colors 
+df <- left_join(df, teamcolors, by = c("team" = "mascot")) 
+
+#Manually adjust some colors, just for readability purposes 
+df <- df %>% 
+  mutate(primary = case_when(
+    team == "Spurs" ~ "#000000",
+    team == "Pacers" ~ "#002D62",
+    team == "Warriors" ~ "#1D428A",
+    team == "Lakers" ~ "#552583",
+    team == "76ers" ~ "#006BB6",
+    team == "Heat" ~ "#41B6E6",
+    team == "Grizzlies" ~ "#00B2A9",
+    team == "Thunder" ~ "#EF3B24",
+    TRUE ~ primary
+  ))
+
+# set up duplicate team column for charting purposes 
+df$teamDuplicate <- df$team 
+
+## My code to remove allstar weekend games
+df <- df %>%
+  filter(team != "Barry" & team != "Durant" & team != "Isiah" & team != "LeBron" &
+           team != "Payton" & team != "Worthy")
+
+# make chart 2
+df %>% 
+  ggplot(aes(x = gameno, y = opp_ou_ra)) + 
+  geom_smooth(data = mutate(df, team = NULL), aes(group = teamDuplicate), method = "lm", formula = y ~ splines::bs(x, 5), se = FALSE, colour = 'grey80', size = .25, alpha = .5) +
+  geom_smooth(aes(group = team, color = primary), method = "lm",  formula = y ~ splines::bs(x, 5), se = FALSE, size = .5, alpha = 1, show.legend = FALSE) +
+  #geom_line(data = mutate(df, team = NULL), aes(group = teamDuplicate), colour = 'grey80', size = .25, alpha = .5) +
+  #geom_line(aes(group = team, color = primary), size = .5, alpha = 1, show.legend = FALSE) +
+  scale_y_continuous(breaks = seq(30, 50, 10)) +
+  scale_x_continuous(breaks = seq(0, 80, 40), limits = c(0, 80, 40)) +
+  scale_color_identity() +
+  facet_wrap(~fct_reorder(team, -team_ou)) +
+  themes() + 
+  theme(plot.title.position = 'plot', 
+        plot.title = element_text(face = 'bold'), 
+        plot.margin = margin(10, 10, 15, 10), 
+        panel.spacing = unit(0.5, 'lines')) +
+  labs(x = "Game Number", 
+       y = "Opponent's Vegas Win Totals", 
+       title = "Opponent's Vegas Win Totals Over A 10-Game Moving Average", 
+       subtitle = "Teams sorted by their 2021-22 Vegas win totals (via Caesars Sportsbook)\nNote: Lines have been smoothed for readability")
 
 
 
